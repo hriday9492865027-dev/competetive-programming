bd94e753bd815a521d8cd8e4e0c71c6750bfb2a7
Hriday <hriday9492865027@gmail.com>
2026-09-06 16:05:17 +0530

    Solved: TELHOME (codechef)

---
+++ codechef/Practice/TELHOME.c
@@ -0,0 +1,8 @@
+// Problem: TELHOME
+// Platform: codechef
+// Language: C​
+// Verdict: Accepted
+// URL: https://www.codechef.com/problems/TELHOME
+// Solved on: 2026-09-06T10:35:15.581Z
+
+// source not captured automatically - open the solution page and copy it manually
