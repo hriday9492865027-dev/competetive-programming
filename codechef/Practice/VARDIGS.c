797ecd3325c83f75487e597b4103f3e8b8413fb8
Hriday <hriday9492865027@gmail.com>
2026-09-06 16:25:28 +0530

    Solved: VARDIGS (codechef)

---
+++ codechef/Practice/VARDIGS.c
@@ -0,0 +1,8 @@
+// Problem: VARDIGS
+// Platform: codechef
+// Language: C​
+// Verdict: Accepted
+// URL: https://www.codechef.com/problems/VARDIGS
+// Solved on: 2026-09-06T10:55:27.165Z
+
+// source not captured automatically - open the solution page and copy it manually
