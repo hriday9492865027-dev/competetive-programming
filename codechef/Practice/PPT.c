82d95a231b1701e45037a6fe5d2ffa644d9e9a3b
Hriday <hriday9492865027@gmail.com>
2026-09-06 16:27:13 +0530

    Solved: PPT (codechef)

---
+++ codechef/Practice/PPT.c
@@ -0,0 +1,8 @@
+// Problem: PPT
+// Platform: codechef
+// Language: C​
+// Verdict: Accepted
+// URL: https://www.codechef.com/problems/PPT
+// Solved on: 2026-09-06T10:57:12.081Z
+
+// source not captured automatically - open the solution page and copy it manually
