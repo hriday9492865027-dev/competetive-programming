4347b28c8301f08ec4a81b9ad4c57a52c041b4f8
Hriday <hriday9492865027@gmail.com>
2026-09-06 16:09:23 +0530

    Solved: FAVWD (codechef)

---
+++ codechef/Practice/FAVWD.c
@@ -0,0 +1,8 @@
+// Problem: FAVWD
+// Platform: codechef
+// Language: C​
+// Verdict: Accepted
+// URL: https://www.codechef.com/problems/FAVWD
+// Solved on: 2026-09-06T10:39:22.922Z
+
+// source not captured automatically - open the solution page and copy it manually
