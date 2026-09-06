2615cea7bb5d64fbd2be6915db04a6a991376f4a
Hriday <hriday9492865027@gmail.com>
2026-09-06 16:14:36 +0530

    Solved: SQPAPER (codechef)

---
+++ codechef/Practice/SQPAPER.c
@@ -0,0 +1,8 @@
+// Problem: SQPAPER
+// Platform: codechef
+// Language: C​
+// Verdict: Accepted
+// URL: https://www.codechef.com/problems/SQPAPER
+// Solved on: 2026-09-06T10:44:35.231Z
+
+// source not captured automatically - open the solution page and copy it manually
