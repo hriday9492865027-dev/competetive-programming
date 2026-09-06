ffa90ee25ae9d879415344306648b2798b06ff7a
Hriday <hriday9492865027@gmail.com>
2026-09-06 16:40:35 +0530

    Solved: RUNCHASE (codechef)

---
+++ codechef/Practice/RUNCHASE.c
@@ -0,0 +1,8 @@
+// Problem: RUNCHASE
+// Platform: codechef
+// Language: C​
+// Verdict: Accepted
+// URL: https://www.codechef.com/problems/RUNCHASE?tab=ide
+// Solved on: 2026-09-06T11:10:34.850Z
+
+// source not captured automatically - open the solution page and copy it manually
