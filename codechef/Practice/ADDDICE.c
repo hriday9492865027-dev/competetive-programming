4128fbf6a4637f2a96c18699c155381c3d74a506
Hriday <hriday9492865027@gmail.com>
2026-09-06 16:19:03 +0530

    Solved: ADDDICE (codechef)

---
+++ codechef/Practice/ADDDICE.c
@@ -0,0 +1,8 @@
+// Problem: ADDDICE
+// Platform: codechef
+// Language: C​
+// Verdict: Accepted
+// URL: https://www.codechef.com/problems/ADDDICE
+// Solved on: 2026-09-06T10:49:02.802Z
+
+// source not captured automatically - open the solution page and copy it manually
