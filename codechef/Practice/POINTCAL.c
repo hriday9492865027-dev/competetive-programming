be6eaccd0c9b886d784936f2a6652f17891047ae
Hriday <hriday9492865027@gmail.com>
2026-09-06 16:12:32 +0530

    Solved: POINTCAL (codechef)

---
+++ codechef/Practice/POINTCAL.c
@@ -0,0 +1,8 @@
+// Problem: POINTCAL
+// Platform: codechef
+// Language: C​
+// Verdict: Accepted
+// URL: https://www.codechef.com/problems/POINTCAL
+// Solved on: 2026-09-06T10:42:31.370Z
+
+// source not captured automatically - open the solution page and copy it manually
