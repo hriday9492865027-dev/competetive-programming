2828a27f8ea7c2d91eb932438c3da3ab375f84ab
Hriday <hriday9492865027@gmail.com>
2026-09-06 16:02:11 +0530

    Solved: SAMESIT (codechef)

---
+++ codechef/Practice/SAMESIT.c
@@ -0,0 +1,8 @@
+// Problem: SAMESIT
+// Platform: codechef
+// Language: C​
+// Verdict: Accepted
+// URL: https://www.codechef.com/problems/SAMESIT
+// Solved on: 2026-09-06T10:32:10.541Z
+
+// source not captured automatically - open the solution page and copy it manually
