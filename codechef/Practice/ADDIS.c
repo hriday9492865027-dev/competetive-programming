43a5798b2f894900a9cf9ab9e9806a5f31e73ed9
Hriday <hriday9492865027@gmail.com>
2026-09-09 21:13:47 +0530

    Solved: ADDIS (codechef)

---
+++ codechef/Practice/ADDIS.c
@@ -0,0 +1,14 @@
+// Problem: ADDIS
+// Platform: codechef
+// Language: C​
+// Verdict: Accepted
+// URL: https://www.codechef.com/START255D/problems/ADDIS
+// Solved on: 2026-09-09T15:43:38.401Z
+
+#include <stdio.h>
+
+int main() {
+	// your code goes here
+
+}
+
