7b684b532513511daaa4adc8c6b027ebdc891024
Hriday <hriday9492865027@gmail.com>
2026-09-09 21:16:49 +0530

    Solved: SPLDM (codechef)

--- codechef/Practice/SPLDM.c
+++ codechef/Practice/SPLDM.c
@@ -3,6 +3,6 @@
 // Language: C​
 // Verdict: Accepted
 // URL: https://www.codechef.com/START255D/problems/SPLDM
-// Solved on: 2026-09-09T15:40:41.744Z
+// Solved on: 2026-09-09T15:46:50.805Z
 
 // source not captured automatically - open the solution page and copy it manually
