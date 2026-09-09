9e601acff474e063b4d38bd3898cbb69eb07102d
Hriday <hriday9492865027@gmail.com>
2026-09-09 20:43:49 +0530

    Solved: EGGBUY (codechef)

--- codechef/Practice/EGGBUY.c
+++ codechef/Practice/EGGBUY.c
@@ -3,6 +3,6 @@
 // Language: C​
 // Verdict: Accepted
 // URL: https://www.codechef.com/START255D/problems/EGGBUY
-// Solved on: 2026-09-09T15:09:21.429Z
+// Solved on: 2026-09-09T15:13:32.936Z
 
-5 8 3
+// source not captured automatically - open the solution page and copy it manually
